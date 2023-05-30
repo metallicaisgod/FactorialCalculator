c4bbd42db4e44f9dff371700d1c0705daa81d64e
Kirill Meshkov <metallicaisgod@yandex.ru>
2023-05-30 12:30:02 +0300

    CoroutineScope and CoroutineContext

--- app/src/main/java/com/kirillmesh/factorialcalculator/MainViewModel.kt
+++ app/src/main/java/com/kirillmesh/factorialcalculator/MainViewModel.kt
@@ -3,10 +3,7 @@ package com.kirillmesh.factorialcalculator
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 import java.math.BigInteger
 
 class MainViewModel : ViewModel() {
@@ -14,28 +11,36 @@ class MainViewModel : ViewModel() {
     private val _state = MutableLiveData<State>()
     val state: LiveData<State>
         get() = _state
+    private val myCoroutine = CoroutineScope(
+        CoroutineName("My coroutine scope") + Dispatchers.Main
+    )
 
     fun calculate(value: String?) {
         if (value.isNullOrBlank()) {
             _state.value = Error
             return
         }
-        viewModelScope.launch {
+        myCoroutine.launch {
             _state.value = Progress
             val number = value.toLong()
-            val result = factorial(number)
+            val result = withContext(Dispatchers.Default) {
+                factorial(number)
+            }
             _state.value = Factorial(result)
         }
     }
 
-    private suspend fun factorial(number: Long): String {
-        return withContext(Dispatchers.Default) {
-            var result = BigInteger.ONE
-            for (i in 1..number) {
-                result = result.multiply(BigInteger.valueOf(i))
-            }
-            result.toString()
+    private fun factorial(number: Long): String {
+        var result = BigInteger.ONE
+        for (i in 1..number) {
+            result = result.multiply(BigInteger.valueOf(i))
         }
+        return result.toString()
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        myCoroutine.cancel()
     }
 
 //    private suspend fun factorial(number: Long): String {
